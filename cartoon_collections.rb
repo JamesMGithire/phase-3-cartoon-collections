c5324d6549d90bbad12975b697f8d25aedce8e7a
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-18 09:47:50 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  puts dwarves.map.with_index{|name, ind|"#{ind+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr# code an argument here
   # Your code here
+  arr.map{|name|"#{name.capitalize!}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls arr# code an argument here
   # Your code here
+  arr.any? {|name| name.length>4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese food# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find{|cheese|food.include?(cheese)}
 end
